a51b44bbdc10d669a356282a8327782b8fee4035
PosaLusa24 <posalusa24@gmail.com>
2020-02-17 22:20:27 +0630

    Testing: MongoDB Integration

--- api/say-hello.js
+++ api/say-hello.js
@@ -1,4 +1,4 @@
 module.exports = (req, res) => {
-  const { name = 'Guys!' } = req.query;
-  res.status(200).send(`Hello ${name}!`);
+  const { name = 'World' } = req.query;
+  res.status(200).send(`Hello ${name} ${process.env.MONGO_URL}!`);
 }
