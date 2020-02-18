84bb84dacda0e549bf3d551e09b025be39465941
PosaLusa24 <posalusa24@gmail.com>
2020-02-18 23:59:18 +0630

    Changed res.send to res.json

--- api/read.js
+++ api/read.js
@@ -3,6 +3,6 @@ const User = require('../mongoose-init').userModel;
 module.exports = (req, res) => {
   User.find((err, users) => {
     if (err) return console.error(err);
-    res.status(200).send(users);
+    res.status(200).json({ users });
   });
 };
