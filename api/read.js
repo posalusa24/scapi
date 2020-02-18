0a7cde0526a8aa4f665f21a49f90ac2dbc76ffa3
PosaLusa24 <posalusa24@gmail.com>
2020-02-19 00:21:21 +0630

    CORS support 2

--- api/read.js
+++ api/read.js
@@ -3,7 +3,7 @@ const User = require('../mongoose-init').userModel;
 module.exports = (req, res) => {
   User.find((err, users) => {
     if (err) return console.error(err);
-    res.setHeader('Access-Control-Allow-Origin', 'https://rfidapp.netlify.com');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.status(200).send(users);
   });
 };
