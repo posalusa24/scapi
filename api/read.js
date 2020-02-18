bf7892ca4fa2908686b70f4a63cdf6841cb15099
PosaLusa24 <posalusa24@gmail.com>
2020-02-19 00:15:25 +0630

    setHeader 2

--- api/read.js
+++ api/read.js
@@ -3,6 +3,7 @@ const User = require('../mongoose-init').userModel;
 module.exports = (req, res) => {
   User.find((err, users) => {
     if (err) return console.error(err);
-    res.setHeader('Access-Control-Allow-Origin', 'https://rfidapp.netlify.com/').status(200).send(users);
+    res.setHeader('Access-Control-Allow-Origin', 'https://rfidapp.netlify.com/');
+    res.status(200).send(users);
   });
 };
