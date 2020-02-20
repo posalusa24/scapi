484e06340d2a39932290f91c59f80dbe6c2bc6ec
PosaLusa24 <posalusa24@gmail.com>
2020-02-20 11:23:51 +0630

    CORS: Used writeHead()

--- api/read.js
+++ api/read.js
@@ -3,7 +3,6 @@ const User = require('../mongoose-init').userModel;
 module.exports = (req, res) => {
   User.find((err, users) => {
     if (err) return console.error(err);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.status(200).send(users);
+    res.writeHead(200, { 'Access-Control-Allow-Origin': '*' }).send(users);
   });
 };
