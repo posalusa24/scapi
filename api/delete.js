d919421e086c136cccac56ec4f44d522a766c1c6
PosaLusa24 <posalusa24@gmail.com>
2020-02-20 12:14:19 +0630

    Added delete all function 3

--- api/delete.js
+++ api/delete.js
@@ -8,7 +8,7 @@ module.exports = (req, res) => {
   });*/
   const query = User.find();
   if (req.query.hasOwnProperty('all')) {
-    query.deleteMany({ uitID: { $regex: '*' } }).exec((err) => {
+    query.deleteMany({ uitID: { $regex: '.' } }).exec((err) => {
       if (err) return console.error(err);
       res.setHeader('Access-Control-Allow-Origin', '*');
       res.status(200).send('Deleted');
