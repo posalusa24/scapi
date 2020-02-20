8166e0df1313f70cfd6d034ca258f57ef0057d37
PosaLusa24 <posalusa24@gmail.com>
2020-02-20 11:27:47 +0630

    CORS: Used middleware

--- api/read.js
+++ api/read.js
@@ -1,8 +1,13 @@
 const User = require('../mongoose-init').userModel;
 
+app.use((req, res, next) => {
+  res.writeHead(200, { 'Access-Control-Allow-Origin': '*' })
+  next();
+});
+
 module.exports = (req, res) => {
   User.find((err, users) => {
     if (err) return console.error(err);
-    res.writeHead(200, { 'Access-Control-Allow-Origin': '*' }).send(users);
+    res.send(users);
   });
 };
