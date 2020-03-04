8127ef3d494d6817108dc490b1a12ef0792fccc0
PosaLusa24 <posalusa24@gmail.com>
2020-03-04 13:58:05 +0630

    Updated check.js

--- api/check.js
+++ api/check.js
@@ -4,13 +4,16 @@ module.exports = (req, res) => {
   User.findOne({ fileToWrite: true }, (err, user) => {
     if (err) return console.error(err);
     res.setHeader('Access-Control-Allow-Origin', '*');
-    if (!user) res.status(200).send("");
-    res.status(200).send({
-      name: user.name,
-      fatherName: user.fatherName,
-      id: user.id,
-      createdDate: user.createdDate,
-      expireDate: user.expireDate
-    });
+    if (user) {
+      res.status(200).send({
+        name: user.name,
+        fatherName: user.fatherName,
+        id: user.id,
+        createdDate: user.createdDate,
+        expireDate: user.expireDate
+      });
+    } else {
+      res.status(200).send("");
+    }
   });
 };
