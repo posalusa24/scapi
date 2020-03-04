70075e75eff24b087045166b4cbbec08479ab0c7
PosaLusa24 <posalusa24@gmail.com>
2020-03-04 16:01:27 +0630

    Added find.js

---
+++ api/find.js
@@ -0,0 +1,19 @@
+const User = require('../mongoose-init').userModel;
+
+module.exports = (req, res) => {
+  User.findOne({ id: req.query.searchid }, (err, user) => {
+    if (err) return console.error(err);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    if (user) {
+      res.status(200).send({
+        name: user.name,
+        fatherName: user.fatherName,
+        id: user.id,
+        createdDate: user.createdDate,
+        expireDate: user.expireDate
+      });
+    } else {
+      res.status(200).send("");
+    }
+  });
+};
